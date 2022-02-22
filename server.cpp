af7cc01f62d6718d112530dc9cb4c7e4a048f3c4
Nicholas Browning <nicholasbrowning@gmail.com>
2022-02-21 23:04:34 -0800

    Im going to sleep

--- server.cpp
+++ server.cpp
@@ -1,12 +1,17 @@
 #include <string>
+#include <cstring>
 #include <thread>
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
 
 void runError(int code);
 void endProgram();
 void makeConnection();
+void makeSocket();
 
 //pointer to our file writers for each connection
 std::vector<std::ofstream*> connections;
@@ -14,17 +19,43 @@ std::vector<std::ofstream*> connections;
 //directory to store files
 std::string direc;
 
+//socket
+int sock;
+int port;
+
+//address
+struct sockaddr_in servaddr;
+
+//TODO process wrong port numbers
 int main(int argc, char** argv)
 {
     if (argc != 3)
     {
         runError(1);
     }
+    port = std::stoi(argv[1]);
     direc = argv[2];
     std::cerr << argv[1] << std::endl << argv[2] << std::endl;
-    makeConnection();
+    makeSocket();
     endProgram();
-    exit(0);
+    return 0;
+}
+
+void makeSocket()
+{
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+    servaddr.sin_port = htons(port);
+
+    if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) == -1)
+    {
+        runError(3);
+    }
+    if ((bind(sock, (const struct sockaddr*)&servaddr, sizeof(servaddr))) == -1)
+    {
+        runError(4);
+    }
 }
 
 //put anything we need to close here
@@ -34,6 +65,7 @@ void endProgram()
     {
         (*connections[x]).close();
     }
+    close(sock);
 }
 
 //run this everytime we make a connection to a client
@@ -51,8 +83,18 @@ void runError(int code)
     switch (code)
     {
     case 1:
-        std::cerr << "ERROR: Incorrect Argument Number!" << std::endl;
+        std::cerr << "ERROR: Incorrect Number of Arguments!" << std::endl;
+        break;
+    case 2:
+        std::cerr << "ERROR: Incorrect Port Number!" << std::endl;
+        break;
+    case 3:
+        std::cerr << "ERROR: Socket Creation Failed!" << std::endl;
+        break;
+    case 4:
+        std::cerr << "ERROR: Bind Creation Failed!" << std::endl;
         break;
     }
+    endProgram();
     exit(code);
 }
