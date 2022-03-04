de7f7bd114460f8441cca5c7edbdb317bfec9971
Nicholas Browning <nicholasbrowning@gmail.com>
2022-03-03 21:00:22 -0800

    Phatty Files, Thicc Thighs, Zero Bugs

--- client.cpp
+++ client.cpp
@@ -105,7 +105,7 @@ int main(int argc, char** argv){
 	uint32_t client_ack_no = 0;
 
 	bool flags[NUM_FLAGS];	// ASF
-
+	
 	// Also updates the seq_no, ack_no, conn_id
 	handshake(sockfd, addr, addr_len, server_seq_no, server_ack_no, connection_id, client_seq_no, client_ack_no, flags);
 
@@ -126,7 +126,22 @@ int main(int argc, char** argv){
 	}
 	struct stat fdStat;
 	fstat(filefd, &fdStat);
-	size_t bytesRead = read(filefd, buf + HEADER_SIZE, fdStat.st_size);
+	size_t bytesRead = 0;
+	int counter = 0;
+	int length;
+	while (counter < fdStat.st_size)
+	{
+		if (fdStat.st_size - counter > MAX_PAYLOAD_SIZE) {
+			bytesRead += read(filefd, buf + HEADER_SIZE, MAX_PAYLOAD_SIZE);
+			length = sendto(sockfd, buf, HEADER_SIZE + MAX_PAYLOAD_SIZE, MSG_CONFIRM, addr, addr_len);
+		}
+		else {
+			bytesRead += read(filefd, buf + HEADER_SIZE, fdStat.st_size - counter);
+			length = sendto(sockfd, buf, HEADER_SIZE + fdStat.st_size - counter, MSG_CONFIRM, addr, addr_len);
+		}
+		counter += MAX_PAYLOAD_SIZE;
+		printClientMessage("SEND", client_seq_no, client_ack_no, connection_id, INITIAL_CWND, INITIAL_SSTHRESH, flags);	
+	}
 	cerr << bytesRead << " bytes read from file" << endl;
 
 	// TODO: TO CHECK IF ALL BYTES OF THE FILE HAVE BEEN ACK'D, MAYBE READ THE ENTIRE FILE INTO A BUFFER,
@@ -136,10 +151,7 @@ int main(int argc, char** argv){
 	// TODO: the send process and receive process can be put in their own functions
 
 	// 								HEADER_SIZE + payload
-	int length = sendto(sockfd, buf, HEADER_SIZE + bytesRead, MSG_CONFIRM, addr, addr_len);
-
-	cerr << "Total bytes sent: " << length << endl;
-	printClientMessage("SEND", client_seq_no, client_ack_no, connection_id, INITIAL_CWND, INITIAL_SSTHRESH, flags);
+	//cerr << "Total bytes sent: " << length << endl;
 
 	memset(buf, '\0', HEADER_SIZE);	
 	memset(flags, '\0', NUM_FLAGS);
